128070e52b3dc8917d3c7d747583fc32c24156f0
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2014-09-16 11:31:36 +0200

    adapted SparqlQueriable to DL-Learner changes in aksw jena sparql api.

--- commons/src/main/java/org/aksw/autosparql/commons/sparql/SparqlQueriable.java
+++ commons/src/main/java/org/aksw/autosparql/commons/sparql/SparqlQueriable.java
@@ -1,18 +1,13 @@
 package org.aksw.autosparql.commons.sparql;
 
-import java.sql.SQLException;
 import java.util.concurrent.TimeUnit;
-
 import org.aksw.jena_sparql_api.cache.core.QueryExecutionFactoryCacheEx;
-import org.aksw.jena_sparql_api.cache.extra.CacheCoreEx;
-import org.aksw.jena_sparql_api.cache.extra.CacheCoreH2;
-import org.aksw.jena_sparql_api.cache.extra.CacheEx;
-import org.aksw.jena_sparql_api.cache.extra.CacheExImpl;
+import org.aksw.jena_sparql_api.cache.extra.CacheFrontend;
+import org.aksw.jena_sparql_api.cache.h2.CacheUtilsH2;
 import org.aksw.jena_sparql_api.core.QueryExecutionFactory;
 import org.aksw.jena_sparql_api.http.QueryExecutionFactoryHttp;
 import org.aksw.jena_sparql_api.model.QueryExecutionFactoryModel;
 import org.dllearner.kb.sparql.SparqlEndpoint;
-
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.Model;
 
@@ -24,16 +19,8 @@ public class SparqlQueriable
 	{			
 		qef = new QueryExecutionFactoryHttp(endpoint.getURL().toString(), endpoint.getDefaultGraphURIs());
 		if(cacheDirectory != null){
-			try {
-				long timeToLive = TimeUnit.DAYS.toMillis(30);
-				CacheCoreEx cacheBackend = CacheCoreH2.create(cacheDirectory, timeToLive, true);
-				CacheEx cacheFrontend = new CacheExImpl(cacheBackend);
-				qef = new QueryExecutionFactoryCacheEx(qef, cacheFrontend);
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+				CacheFrontend frontend = CacheUtilsH2.createCacheFrontend("sparqlquerieable", false, TimeUnit.DAYS.toMillis(7));
+				qef = new QueryExecutionFactoryCacheEx(qef, frontend);
 		}
 	}
 
